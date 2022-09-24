cfff6053dccc3584c01c602272062166d5ec20ba
gabesayshi <gabesayshi@gmail.com>
2022-09-24 16:05:03 -0400

    adopt default vpc and renamed file

--- prod.tf
+++ prod.tf
@@ -3,7 +3,9 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "tf_course" {
+resource "aws_s3_bucket" "prod_tf_course" {
   bucket = "my-tf-bucket-23127894263"
   acl = "private"
 }
+
+resource"aws_default_vpc" "default" {}
